20e2dfc4706dac9512dd1b3186a3c0a458ff84d6
chris o'hara <crsohara@gmail.com>
2022-05-26 10:46:14 +0300

    Fix: iOS safarai AddModal overflow with long magnet input

--- src/components/Modals/AddModal.vue
+++ src/components/Modals/AddModal.vue
@@ -22,7 +22,7 @@
       @dragleave.prevent="DragLeave"
     />
     <v-card
-      style="height:100%"
+      style="min-height:100%"
       :class="showWrapDrag ? 'wrap-drag' : ''"
       @drop.prevent="addDropFile"
       @dragover.prevent="showWrapDrag = true"
